355c280e71e3649b3a0d5fa54c45df6a688be8ca
Victor_Kochukov <victorkochukow90@gmail.com>
2020-12-04 03:22:38 +0300

    New functions in file

---
+++ second.py
@@ -0,0 +1,11 @@
+n1 = int(input("Введите первое число: "))
+n2 = int(input("Введите второе число: "))
+
+raznost1 = 0
+raznost2 = 0
+
+raznost1 = n1 - n2
+raznost2 = n2 - n1
+
+print("Разность первого и второго числа:", raznost1)
+print("Разность второго и первого числа:", raznost2)
